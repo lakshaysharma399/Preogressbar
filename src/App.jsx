812b696bf991367683a1b384fcbb53f4a1e3c519
lakshaysharma399 <lakshaysharma399@gmail.com>
2026-02-22 19:15:07 +0530

    intital commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,42 +7,11 @@ import accordionData from "./Data/Data";
 function App() {
   const [id, setid] = useState([]);
 
-  function func(index){
-
-
-    if(id.includes(index)){
-     const arr = id.filter((val) => {
-        return val!=index;
-      })
-
-      setid(arr);
-
-
-    }else{
-
-      setid([...id, index]);
-
-    }
-
-  }
+  
 
   return (
     <>
-      <div className="main-div">
-        <div className="innerdiv">
-          {accordionData.map((val, index) => (
-            <div key={index}>
-              <div onClick={() => func(index)} className="accodio-heading">
-                {val.question}
-              </div>
-
-              {id.includes(index) && (
-                <div className="accordion-content">{val.answer}</div>
-              )}
-            </div>
-          ))}
-        </div>
-      </div>
+    
     </>
   );
 }
