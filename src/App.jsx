5cc7c5406f69a96360c7bf828e3a0f93ec42146e
lakshaysharma399 <lakshaysharma399@gmail.com>
2026-02-22 22:02:10 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
-import accordionData from "./Data/Data";
 
 function App() {
-  const [id, setid] = useState([]);
+  const [id, setid] = useState(0);
+  const [input, setInput] = useState(0);
 
-  
+  useEffect(() => {
+    if (id >= input) return;
+
+    const timer = setTimeout(() => {
+      setid(prev => prev + 1);
+    }, 10);
+
+    return () => clearTimeout(timer);
+  }, [id, input]);
+
+  function startProgress() {
+    setid(0); // reset and start again
+  }
 
   return (
     <>
-    
+      <div className="outter">
+        <div style={{ width: `${id}%` }} className="inner">
+          {id}%
+        </div>
+      </div>
+
+      <div>
+        <input
+          type="number"
+          value={input}
+          onChange={(e) => setInput(Number(e.target.value))}
+        />
+        <button onClick={startProgress}>Start</button>
+      </div>
     </>
   );
 }
